9ecb791571e06e10b699bf9e2d68894c48d506f4
aniketbhore <bhore.a@northeastern.edu>
2023-04-10 11:00:55 -0400

    Modified and Implemented Functions

--- Functions.sql
+++ Functions.sql
@@ -48,3 +48,34 @@ BEGIN
   
   RETURN units_with_pets;
 END;
+---------Get Total Security Deposit Amount for a Lease
+CREATE OR REPLACE FUNCTION get_security_deposit_total(lease_no IN NUMBER)
+  RETURN NUMBER
+IS
+  deposit_total NUMBER;
+BEGIN
+  SELECT SUM(SECURITY_DEPOSIT)
+  INTO deposit_total
+  FROM LEASE_AGREEMENT
+  WHERE LEASE_NO = lease_no;
+  
+  RETURN deposit_total;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    DBMS_OUTPUT.PUT_LINE('No lease found with lease number ' || lease_no);
+    RETURN NULL;
+END;
+
+-------------Get Open Maintenance Requests
+CREATE OR REPLACE FUNCTION get_open_maintenance_requests
+  RETURN SYS_REFCURSOR
+IS
+  open_requests SYS_REFCURSOR;
+BEGIN
+  OPEN open_requests FOR
+    SELECT *
+    FROM REQUESTS
+    WHERE STATUS = 'OPEN';
+  
+  RETURN open_requests;
+END;
